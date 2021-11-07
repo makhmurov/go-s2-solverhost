4d1bccdca49adea6998b800943af0e0c4460ca31
Andrey Makhmurov <makhmurov@gmail.com>
2021-11-07 20:18:11 +0300

    Comment out temporary unused code

--- main.go
+++ main.go
@@ -1,34 +1,27 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
 	"solverhost/verify"
-	"syscall"
 )
 
 func main() {
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	// interrupt := make(chan os.Signal, 1)
+	// signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	_ = ctx
+	// ctx, cancel := context.WithCancel(context.Background())
+	// defer cancel()
 
 	verify.Do()
 
-	/*
-		for {
-			select {
-			case <-ctx.Done():
-				log.Println("all done, shutdown")
-				return
-			case <-interrupt:
-				log.Println("interrupt, shutdown")
-				cancel()
-				return
-			}
-		}
-	*/
+	// for {
+	// 	select {
+	// 	case <-ctx.Done():
+	// 		log.Println("all done, shutdown")
+	// 		return
+	// 	case <-interrupt:
+	// 		log.Println("interrupt, shutdown")
+	// 		cancel()
+	// 		return
+	// 	}
+	// }
 }
